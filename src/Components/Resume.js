cef225a23021e522364ddd10a5b69f4df948fad2
negar zeynalabdin <n.zeyn10@gmail.com>
2022-07-25 08:34:33 +0430

    change somthing.

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,75 +3,92 @@ import React, { Component } from 'react';
 class Resume extends Component {
   render() {
 
-    if(this.props.data){
+    if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
+      var education = this.props.data.education.map(function (education) {
         return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+          <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+          <p>{education.description}</p></div>
       })
-      var work = this.props.data.work.map(function(work){
+      var work = this.props.data.work.map(function (work) {
         return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
+          <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+          <p>{work.description}</p>
         </div>
       })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+      var experience = this.props.data.TeachingExperience.map(function (TeachingExperience) {
+        return <div key={TeachingExperience.course}><h3><a href={TeachingExperience.url}>{TeachingExperience.course}</a></h3>
+          <p className="info">{TeachingExperience.notes}<span>&bull;</span> <em className="date">{TeachingExperience.date}</em></p>
+          <p></p>
+        </div>
+      })
+      var skills = this.props.data.skills.map(function (skills) {
+        var className = 'bar-expand ' + skills.name.toLowerCase();
+        return <li key={skills.name}><span style={{ width: skills.level }} className={className}></span><em>{skills.name}</em></li>
       })
     }
 
     return (
       <section id="resume">
 
-      <div className="row education">
-         <div className="three columns header-col">
+        <div className="row education">
+          <div className="three columns header-col">
             <h1><span>Education</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
             <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+              <div className="twelve columns">
+                {education}
+              </div>
             </div>
-         </div>
-      </div>
+          </div>
+        </div>
 
 
-      <div className="row work">
+        <div className="row work">
+
+          <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            {work}
+          </div>
+        </div>
+
+        <div className="row experience">
 
          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
+            <h1><span>Experience</span></h1>
          </div>
 
          <div className="nine columns main-col">
-          {work}
+          {experience}
         </div>
     </div>
 
 
 
-      <div className="row skill">
+        <div className="row skill">
 
-         <div className="three columns header-col">
+          <div className="three columns header-col">
             <h1><span>Skills</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
 
             <p>{skillmessage}
             </p>
 
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
+            <div className="bars">
+              <ul className="skills">
+                {skills}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
